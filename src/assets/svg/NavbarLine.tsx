12030bf64d3073133511e6d612c695eefbcff4c3
Digudev <1.5.dg.diego@gmail.com>
2023-08-23 12:52:11 -0600

    CHORES: Fix name properties

--- src/assets/svg/NavbarLine.tsx
+++ src/assets/svg/NavbarLine.tsx
@@ -13,8 +13,8 @@ const NavbarLine = () => {
 			<path
 				d="M-18.8075 13.4759C-18.8075 13.4759 804.147 4.03309 868.104 4.57977C932.061 5.12645 1033.29 39.9446 1058.64 43.6564C1083.99 47.3683 1435.28 46.8758 1435.28 46.8758"
 				stroke="url(#paint0_linear_119_10)"
-				stroke-opacity="0.28"
-				stroke-width="8"
+				strokeOpacity="0.28"
+				strokeWidth="8"
 			/>
 			<defs>
 				<linearGradient
@@ -25,9 +25,9 @@ const NavbarLine = () => {
 					y2="91.0001"
 					gradientUnits="userSpaceOnUse"
 				>
-					<stop offset="0.453125" stop-color="#00C9A7" />
-					<stop offset="0.723958" stop-color="#845EC2" />
-					<stop offset="1" stop-color="#845EC2" />
+					<stop offset="0.453125" stopColor="#00C9A7" />
+					<stop offset="0.723958" stopColor="#845EC2" />
+					<stop offset="1" stopColor="#845EC2" />
 				</linearGradient>
 			</defs>
 		</svg>
